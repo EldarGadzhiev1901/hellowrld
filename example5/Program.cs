946e7dffefcbbcef9085f73442bfccde04654f1b
Eldar Gadzhiev <mrcartman65@gmail.com>
2023-02-09 20:40:28 +0300

    exp5

---
+++ example5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("enter your name:");
+string username = Console.ReadLine();
+
+if(username == "Tony")
+{
+    Console.WriteLine("Dungeon master, welcome");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+
+}
